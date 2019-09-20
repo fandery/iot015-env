e8540459ad6a20a15ddd46168d7a402df30b36b2
Felipe Andery Reis <fandery@inatel.br>
2019-09-20 15:06:39 -0300

    change repository name

--- prepare-env.sh
+++ prepare-env.sh
@@ -15,7 +15,7 @@ cd ~/work
 if [ -d "$ENVPATH" ]; then
 	rm -rf $ENVPATH
 fi
-git clone --recursive https://github.com/dmazzer/torcs-telemetry-bigdata.git
+git clone --recursive https://github.com/fandery/torcs-telemetry-bigdata.git
 cp ~/work/bigdata-tools/udp-kafka-bridge/udp-kafka-bridge-assembly-0.1.jar $ENVPATH/udp-kafka-bridge/
 
 echo "generating ssh keys for hadoop"
